5a860b78c723d21c0f8c38fb8ad9210d0aac1d12
Amartya Kumar <21ucs018@lnmiit.ac.in>
2025-01-21 20:59:51 +0530

    Made review requests api endpoints

--- src/routes/request.js
+++ src/routes/request.js
@@ -65,4 +65,55 @@ requestRouter.post('/request/send/:status/:userId',userAuth,async(req,res) => {
 })
 
 
+requestRouter.post('/request/review/:status/:requestId',userAuth,async(req,res) => {
+
+ try{
+
+   // Akshay(oneWhoSentARequest) --> Virat(loogedInUser) 
+
+  const {status,requestId} = req.params
+  const loggedInUser = req.user
+
+  // Check status first
+  const allowedStatus = ['accepted','rejected'];
+  if(!allowedStatus.includes(status)){
+    return res.status(400).json({
+      message : "Invalid STATUS"
+    })
+  }
+
+
+  // Check for requestId and if the user is 'interested' and is a loggedInUser
+  const userData = await ConnectionRequest.findOne({
+    _id : requestId,
+    toUserId : loggedInUser._id,
+    status : "interested",
+  })
+
+  if(!userData){
+    return res.status(400).json({
+      message : "No such USER exist"
+    })
+  }
+
+  userData.status = status;
+
+  console.log(userData.status);
+
+  await userData.save();
+  
+
+  res.json({
+    message : `Connection ${status} succesfully`,
+    userData
+  })
+
+
+ }catch(err){
+  res.status(400).send('ERROR: ' + err.message)
+ }
+
+})
+
+
 module.exports = requestRouter;
